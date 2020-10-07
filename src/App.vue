4654f681fe5ee7f82883965aef9bfeb3d38d4de5
xjjabb <lh2110738476@163.com>
2020-10-07 18:21:11 +0800

    初始化项目

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
-    123
+     <el-button>默认按钮</el-button>
+  <el-button type="primary">主要按钮</el-button>
+  <el-button type="success">成功按钮</el-button>
+  <el-button type="info">信息按钮</el-button>
+  <el-button type="warning">警告按钮</el-button>
+  <el-button type="danger">危险按钮</el-button>
   </div>
 </template>
 
@@ -15,7 +20,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less">
 #app {
 
 }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import less from 'less'
+Vue.use(less)
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+import './assets/css/base.css';
+import Axios from 'axios';
+
 Vue.config.productionTip = false
 
 new Vue({
